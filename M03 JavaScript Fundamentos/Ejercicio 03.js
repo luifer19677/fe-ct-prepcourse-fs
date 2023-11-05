957483c667481f6dca4e1ddbad2a35c1aa2d1b2e
unknown <lfquintero19677@gmail.com>
2023-11-05 12:15:05 -0500

    Solución Ejercicio 03.js_M03 JavaScript

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,37 +5,80 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y){
+      return true;
+  }
+  else{
+      return false;
+  }
 }
+var compara = sonIguales(5,5);
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1.length === str2.length){
+      return true;
+  }
+  else {
+      return false;
+  }
+  var longitud = tienenMismaLongitud('hola', 'a todos');
+
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90 ){
+      return true;
+  }
+  else {
+      return false;
+  }
 }
+var numero = menosQueNoventa(80);
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num  > 50 ){
+      return true;
+  }
+  else {
+      return false;
+  }
 }
+var numero = mayorQueCincuenta(40);
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 === 0){
+      return true;
+  }
+  else {
+      return false;
+  }
 }
+var numero = esPar(289);
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 != 0){
+      return true;
+  }
+  else {
+      return false;
+  }
 }
+var numero = esImpar(567);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
