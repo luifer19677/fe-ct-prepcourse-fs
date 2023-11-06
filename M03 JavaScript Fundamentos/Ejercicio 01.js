9c1b00b02ee7f63a2b057c45d089b42d97c62ecb
unknown <lfquintero19677@gmail.com>
2023-11-06 14:08:45 -0500

    M03 Ejercicio 01.js

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = 'Luis Fernando Quintero';
+const nuevoString = 'Fernando';
 
 // Crea una variable de tipo number.
 const nuevoNumero = 125;
