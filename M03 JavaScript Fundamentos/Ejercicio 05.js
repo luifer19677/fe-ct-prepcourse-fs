b82bd5fa6a79234d3468671e0b2f9c0a31a7781d
unknown <lfquintero19677@gmail.com>
2023-11-06 13:58:55 -0500

    Ejercicio 05.js

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -32,7 +32,7 @@ function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
-   return (nombre + ' ' + apellido);
+   return nombre + ' ' + apellido;
 }
 //var combinar = combinarNombres('fernando','quintero');
 
