e12bbb3c715de93e9dddd66f87f56ee4f6ea7018
unknown <lfquintero19677@gmail.com>
2023-11-06 14:14:11 -0500

    M03 Ejercicio03.js

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -70,7 +70,7 @@ function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
-   if (num % 2 != 0){
+   if (num % 2 !== 0){
       return true;
   }
   else {
