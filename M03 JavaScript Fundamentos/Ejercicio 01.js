363fac74690b6d99006137a545eb45f384190d79
FERNANDO QUINTERO <lfquintero19677>
2024-06-07 12:39:03 -0500

    first commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,13 +5,13 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = 'Luis Fernando';
+const nuevoString = 'Luis Fernando Quintero';
 
 // Crea una variable de tipo number.
-const nuevoNumero = 125;
+const nuevoNumero = 12;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = false;
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - 7 === 3;
