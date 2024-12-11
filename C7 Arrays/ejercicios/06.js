f8b5736acdcd9e0a9b06a8c5ea25745c5dfe2edc
FQuintero <lfquintero19677@gmail.com>
2024-12-11 08:45:44 -0500

    ejercicio 06.js

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -1,6 +1,11 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+  return array.reverse();
 }
+// Pruebas
+console.log(invertirArray([1, 2, 3, 4])); // [4, 3, 2, 1]
+console.log(invertirArray(['azul', 'rojo', 'verde'])); // ['verde', 'rojo', 'azul']
+console.log(invertirArray([])); // []
 
 module.exports = invertirArray;
