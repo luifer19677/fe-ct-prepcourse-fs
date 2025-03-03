89a5d0767cbc1b69e7dc2880fdf823cd171c760d
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-03 11:39:01 -0500

    commit 04.js

--- C5 Fundamentos Javascript/ejercicios/04.js
+++ C5 Fundamentos Javascript/ejercicios/04.js
@@ -1,6 +1,6 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevaResta = 10 - 7 === 3;
+const nuevaResta = 20 - 7 === 13;
 
 module.exports = nuevaResta;
