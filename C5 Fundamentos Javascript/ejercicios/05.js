d44a6de22cb6f8279fa0336050c19ceaaf33fe69
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-03 11:40:15 -0500

    commit 05.js

--- C5 Fundamentos Javascript/ejercicios/05.js
+++ C5 Fundamentos Javascript/ejercicios/05.js
@@ -1,6 +1,6 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevaMultiplicacion = 10 * 4 === 40;
+const nuevaMultiplicacion = 100 * 4 === 400;
 
 module.exports = nuevaMultiplicacion;
