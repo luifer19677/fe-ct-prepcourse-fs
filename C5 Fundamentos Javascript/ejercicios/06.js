a1c064fe811e401c4c7993852c8f1458e3150a68
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-03 11:45:28 -0500

    06.js

--- C5 Fundamentos Javascript/ejercicios/06.js
+++ C5 Fundamentos Javascript/ejercicios/06.js
@@ -1,6 +1,6 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoModulo = 21 % 5 === 1;
+const nuevoModulo = 22 % 5 === 2;
 
 module.exports = nuevoModulo;
