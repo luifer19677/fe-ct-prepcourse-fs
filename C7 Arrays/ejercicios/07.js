c3271c2ce112855418a6724ff1cb850f02d72f2b
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-04 18:13:13 -0500

    07.js

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,9 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  return array.sort();
 }
+let frutas = ['pera', 'manzana', 'pitaya', 'mango', 'uva', 'anon'];
+console.log(ordenarArray(frutas));
 
 module.exports = ordenarArray;
