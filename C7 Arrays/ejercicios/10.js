14797c6c5a5b27b255177be7bc4f6ba82997d6ce
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-04 18:29:28 -0500

    commit 10.js

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,10 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  // Usamos find() para encontrar el primer string con más de 5 caracteres
+  return array.find(elemento => typeof elemento === "string" && elemento.length > 5) || null;
 }
+console.log(obtenerPrimerStringLargo(["hola", "mundo", "JavaScript", "code"])); // "JavaScript"
+console.log(obtenerPrimerStringLargo(["hi", "ok", "no"])); // null
 
 module.exports = obtenerPrimerStringLargo;
