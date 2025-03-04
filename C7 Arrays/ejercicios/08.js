1045b18265d0d3c5d0929bc511c1eb42e815d8d2
Fernando Quintero <lfquintero19677@gmail.com>
2025-03-04 18:16:21 -0500

    commit 08.js

--- C7 Arrays/ejercicios/08.js
+++ C7 Arrays/ejercicios/08.js
@@ -3,6 +3,10 @@ function encontrarElemento(num, array) {
    // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
    // Si no se encuentra, retorna -1.
    // Tu código:
+   // Usamos indexOf para buscar el índice del número en el array.
+   return array.indexOf(num);
 }
+console.log(encontrarElemento(5, [1, 2, 3, 4, 5])); // 4
+console.log(encontrarElemento(10, [1, 2, 3, 4, 5])); // -1
 
 module.exports = encontrarElemento;
